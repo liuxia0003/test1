d5671e308a2ccdfee090d6c7d32e5c8230696d84
liuxia0003 <54950012+liuxia0003@users.noreply.github.com>
2019-09-08 22:25:12 +0800

    week01

---
+++ UserInterface.java
@@ -0,0 +1,19 @@
+package paint;
+
+import javax.swing.JFrame;
+public class UserInterface extends JFrame 
+{
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) 
+	{
+		JFrame frame = new JFrame("Simple Paint App");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		DrawingPanel paintPanel = new DrawingPanel();
+		ButtonPanel mainPanel = new ButtonPanel(paintPanel);
+		frame.getContentPane().add(mainPanel);
+		frame.setVisible(true);
+		frame.pack();
+	}	
+}
